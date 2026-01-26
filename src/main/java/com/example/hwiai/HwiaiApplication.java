2bb3a3e77bee70726bf42499a0a40a8499ce8042
hhhhhwi <aab555586@gmail.com>
2026-01-26 23:06:24 +0900

    Initial commit: Spring Boot AI practice project

---
+++ src/main/java/com/example/hwiai/HwiaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwiaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwiaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hwiai/HwiaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwiaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
