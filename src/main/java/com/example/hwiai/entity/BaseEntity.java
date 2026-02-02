12df0f9f1bd9a76c80b2903a6063a87f2ed7b0cb
hhhhhwi <aab555586@gmail.com>
2026-02-03 07:27:31 +0900

    fix: createdDate와 updatedDate 필드가 null로 남는 버그 수정

--- src/main/java/com/example/hwiai/entity/BaseEntity.java
+++ src/main/java/com/example/hwiai/entity/BaseEntity.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
@@ -16,5 +19,5 @@ public abstract class BaseEntity {
     
 
     @LastModifiedDate
-    private LocalDateTime updateDate;
+    private LocalDateTime updatedDate;
 }
