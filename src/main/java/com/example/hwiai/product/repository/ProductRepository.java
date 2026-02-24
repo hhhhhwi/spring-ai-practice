9654a7d6ccacf1969134cc810d78853775533c52
hhhhhwi <aab555586@gmail.com>
2026-02-24 22:57:51 +0900

    refactor: JpaRepository로 변경

--- src/main/java/com/example/hwiai/product/repository/ProductRepository.java
+++ src/main/java/com/example/hwiai/product/repository/ProductRepository.java
@@ -1,10 +1,10 @@
 package com.example.hwiai.product.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.hwiai.product.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
